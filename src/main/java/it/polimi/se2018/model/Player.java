088772ed09fcda683cc4a42a97dafac0f56e1484
ontech7 <dontrok1@hotmail.it>
2018-05-03 19:45:15 +0200

    Implemented 'Player', 'Score' classes - Added setter and getter of 'activatedToolcard' attribute from 'Player' class

--- src/main/java/it/polimi/se2018/model/Player.java
+++ src/main/java/it/polimi/se2018/model/Player.java
@@ -5,38 +5,86 @@ public class Player {
     private Board board;
     private int token;
     private String name;
-    private PrivateObjCard privateObjCards;
+    private PrivateObjCard privateObjCard;
     private ToolCard activatedToolcard;
 
-    public Player() {}
+    public Player() {
+        board = null;
+        token = 0;
+        name = null;
+        privateObjCard = null;
+        activatedToolcard = null;
+    }
 
-    //gets the board of the player
+    public Player(Board newBoard, int newToken, String newName, PrivateObjCard newPrivateObjCard, ToolCard newActivatedToolcard) {
+        board = newBoard;
+        token = newToken;
+        name = newName;
+        privateObjCard = newPrivateObjCard;
+        activatedToolcard = newActivatedToolcard;
+    }
+    
+    /**
+     * Getter of the board of the player
+     * @return the current player board
+     */
     public Board getBoard() {
-
+        return board;
     }
 
-    //gets the remaining tokens of the player
+    /**
+     * Getter of the remaining tokens of the player
+     * @return the current player tokens
+     */
     public int getToken() {
-
+        return token;
     }
 
-    //sets the tokens of the player
+    /**
+     * Setter of the tokens of the player
+     * @param newToken object to set
+     */
     public void setToken(int newToken) {
-
+        token = newToken;
     }
 
-    //gets the name chosen by the player
+    /**
+     * Getter of the name chosen by the player
+     * @return the current player name
+     */
     public String getName() {
-
+        return name;
     }
 
-    //gets the private objective card that the player received at the start of the match
+    /**
+     * Getter of the private objective card that the player received at the start of the match
+     * @return the current player private objective card
+     */
     public PrivateObjCard getPrivateObjCard() {
-
+        return privateObjCard;
     }
 
-    //sets the private objective card of the player
+    /**
+     * Setter of the private objective card of the player
+     * @param card object to set
+     */
     public void setPrivateObjCard(PrivateObjCard card) {
+        privateObjCard = card;
+    }
 
+    /**
+     * Getter of the toolcard activated by the player
+     * @return the current toolcard used
+     */
+    public ToolCard getActivatedToolcard() {
+        return activatedToolcard;
+    }
+
+    /**
+     * Setter of the toolcard activated by the player
+     * @param newActivatedToolcard object to set
+     */
+    public void setActivatedToolcard(ToolCard newActivatedToolcard) {
+        activatedToolcard = newActivatedToolcard;
     }
 }
--- src/main/java/it/polimi/se2018/model/Score.java
+++ src/main/java/it/polimi/se2018/model/Score.java
@@ -7,20 +7,51 @@ public class Score {
     private int tokens;
     private int emptyElems;
 
-    public Score() {}
+    public Score() {
+        privateObjCards = 0;
+        publicObjCards = 0;
+        tokens = 0;
+        emptyElems = 0;
+    }
 
-    //gets all the 4 score values of the player
+    public Score(int privCards, int publCards, int tok, int empty) {
+        privateObjCards = privCards;
+        publicObjCards = publCards;
+        tokens = tok;
+        emptyElems = empty;
+    }
+
+    /**
+     * Getter of all the score values of the player
+     * @return the 4 scores
+     */
     public int[] getValues() {
+        int[] ret = {privateObjCards, publicObjCards, tokens, emptyElems};
 
+        return ret;
     }
 
     //sets all the 4 score values of the player
-    public void setValues(int privCards, int publCards, int tok, int emptyElems) {
 
+    /**
+     * Setter of all the score values of the player
+     * @param privCards score based on private objective cards
+     * @param publCards score based on public objective cards
+     * @param tok score based on remaining tokens
+     * @param empty score based on empty cells of the player board
+     */
+    public void setValues(int privCards, int publCards, int tok, int empty) {
+        privateObjCards = privCards;
+        publicObjCards = publCards;
+        tokens = tok;
+        emptyElems = empty;
     }
 
-    //gets the overall score of the player
+    /**
+     * Getter of the overall score of the player
+     * @return the sum of the 4 scores
+     */
     public int getOverallScore() {
-
+        return publicObjCards + privateObjCards + tokens - emptyElems;
     }
 }
