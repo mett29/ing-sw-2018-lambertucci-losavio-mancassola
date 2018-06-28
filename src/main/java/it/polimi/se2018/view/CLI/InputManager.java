d95eb454a138b23a8be7c8caf06a201a64b64e18
Michele Lambertucci <michele.lambertucci1@gmail.com>
2018-06-29 00:32:21 +0200

    Attempted to fix input handling in CLI (#47) Need testing

--- src/main/java/it/polimi/se2018/view/CLI/InputManager.java
+++ src/main/java/it/polimi/se2018/view/CLI/InputManager.java
@@ -7,6 +7,9 @@ import it.polimi.se2018.network.client.BoardCoordMove;
 import it.polimi.se2018.network.client.Client;
 import it.polimi.se2018.network.client.DiceContainerCoordMove;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Scanner;
@@ -32,19 +35,24 @@ public class InputManager {
     public static void ask(Pattern accepted, Function<String, Integer> converter, Consumer<Integer> onSelected){
         getInstance().thread = new Thread(() -> {
             int selected = -1;
-            Scanner scanner = new Scanner(System.in);
-            while(selected ==-1){
-                if(getInstance().thread.isInterrupted()){
-                    return;
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            Scanner scanner = new Scanner(bufferedReader);
+            try {
+                while (selected == -1) {
+                    while (!bufferedReader.ready()) {
+                        Thread.sleep(100);
+                    }
+                    if (scanner.hasNext(accepted)) {
+                        String input = scanner.next(accepted);
+                        selected = converter.apply(input);
+                        onSelected.accept(selected);
+                    } else {
+                        scanner.next();
+                        selected = -1;
+                    }
                 }
-                if (scanner.hasNext(accepted)) {
-                    String input = scanner.next(accepted);
-                    selected = converter.apply(input);
-                    onSelected.accept(selected);
-                } else {
-                    scanner.next();
-                    selected = -1;
-                }
+            } catch(IOException|InterruptedException e){
+                // do nothing: thread closes
             }
         });
 
@@ -53,40 +61,45 @@ public class InputManager {
 
     public static void askBoard(Board board, EnumSet<CellState> cellStates, Client client){
         getInstance().thread = new Thread(() -> {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
             Pattern ptn = Pattern.compile("[A-T]?", Pattern.CASE_INSENSITIVE);
-            Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(bufferedReader);
             int selection = -1;
-            while(selection == -1){
-                if (getInstance().thread.isInterrupted()) {
-                    return;
-                }
-                if(sc.hasNext("undo")){
-                    client.sendUndoRequest();
-                }
-                if(sc.hasNext(ptn)) {
-                    String found = sc.next(ptn);
-                    selection = selectionMap.indexOf(found.toUpperCase().charAt(0));
-                    if(!(CLI.Stringifier.acceptedCell(board, selection % 5, selection / 5, cellStates))){
-                        String message = "The cell you selected is not acceptable. S" +
-                                CLI.Stringifier.toString(cellStates) +
-                                " (type the corresponding character)";
-                        System.out.println(message);
+            try {
+                while (selection == -1) {
+                    while (!bufferedReader.ready()) {
+                        Thread.sleep(100);
+                    }
+                    if (sc.hasNext("undo")) {
+                        client.sendUndoRequest();
+                    }
+                    if (sc.hasNext(ptn)) {
+                        String found = sc.next(ptn);
+                        selection = selectionMap.indexOf(found.toUpperCase().charAt(0));
+                        if (!(CLI.Stringifier.acceptedCell(board, selection % 5, selection / 5, cellStates))) {
+                            String message = "The cell you selected is not acceptable. S" +
+                                    CLI.Stringifier.toString(cellStates) +
+                                    " (type the corresponding character)";
+                            System.out.println(message);
+                            selection = -1;
+                        }
+                    } else {
+                        StringBuilder buffer = new StringBuilder();
+                        buffer.append("The cell you selected is not acceptable. S");
+                        buffer.append(CLI.Stringifier.toString(cellStates));
+                        buffer.append(" (type the corresponding character)");
+                        System.out.println(buffer.toString());
+                        sc.next();
                         selection = -1;
                     }
-                } else {
-                    StringBuilder buffer = new StringBuilder();
-                    buffer.append("The cell you selected is not acceptable. S");
-                    buffer.append(CLI.Stringifier.toString(cellStates));
-                    buffer.append(" (type the corresponding character)");
-                    System.out.println(buffer.toString());
-                    sc.next();
-                    selection = -1;
                 }
-            }
-            int x = selection % 5;
-            int y = selection / 5;
+                int x = selection % 5;
+                int y = selection / 5;
 
-            client.sendMove(new BoardCoordMove(x, y));
+                client.sendMove(new BoardCoordMove(x, y));
+            } catch(IOException|InterruptedException e){
+                // do nothing: thread closes
+            }
         });
 
         getInstance().thread.start();
@@ -94,51 +107,61 @@ public class InputManager {
 
     public static void askDiceContainer(DiceContainer diceContainer, EnumSet<CellState> cellStates, boolean isDraftPool, Client client){
         getInstance().thread = new Thread(() -> {
-            Scanner sc = new Scanner(System.in);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            Scanner sc = new Scanner(bufferedReader);
             Pattern ptn = Pattern.compile("[A-T]?", Pattern.CASE_INSENSITIVE);
             int selection = -1;
-            while(selection == -1){
-                if(getInstance().thread.isInterrupted()){
-                    return;
-                }
-                if(sc.hasNext("undo")){
-                    client.sendUndoRequest();
-                }
-                if(sc.hasNext(ptn)){
-                    String found = sc.next(ptn);
-                    selection = selectionMap.indexOf(found.toUpperCase().charAt(0));
-                    if(selection < 0 || selection >= diceContainer.getMaxSize() || !CLI.Stringifier.acceptedCell(diceContainer, selection, cellStates)){
-                        System.out.println("Unacceptable selection. " + CLI.Stringifier.pickContainerMessage(cellStates));
-                        selection = -1;
+            try {
+                while (selection == -1) {
+                    while (!bufferedReader.ready()) {
+                        Thread.sleep(100);
                     }
-                }  else {
-                    sc.next();
-                    System.out.println("Unacceptable selection. " + CLI.Stringifier.pickContainerMessage(cellStates));
+                    if (sc.hasNext("undo")) {
+                        client.sendUndoRequest();
+                    }
+                    if (sc.hasNext(ptn)) {
+                        String found = sc.next(ptn);
+                        selection = selectionMap.indexOf(found.toUpperCase().charAt(0));
+                        if (selection < 0 || selection >= diceContainer.getMaxSize() || !CLI.Stringifier.acceptedCell(diceContainer, selection, cellStates)) {
+                            System.out.println("Unacceptable selection. " + CLI.Stringifier.pickContainerMessage(cellStates));
+                            selection = -1;
+                        }
+                    } else {
+                        sc.next();
+                        System.out.println("Unacceptable selection. " + CLI.Stringifier.pickContainerMessage(cellStates));
+                    }
                 }
+                client.sendMove(new DiceContainerCoordMove(selection,
+                        isDraftPool ? DiceContainerCoordMove.DiceContainerName.DRAFT_POOL : DiceContainerCoordMove.DiceContainerName.ROUND_TRACKER));
+            } catch(IOException|InterruptedException e){
+                // do nothing: thread closes
             }
-            client.sendMove(new DiceContainerCoordMove(selection,
-                    isDraftPool ? DiceContainerCoordMove.DiceContainerName.DRAFT_POOL : DiceContainerCoordMove.DiceContainerName.ROUND_TRACKER));
         });
         getInstance().thread.start();
     }
 
     public static void ask(List selectables, Consumer<Integer> onSelected){
         getInstance().thread = new Thread(() -> {
-            Scanner sc = new Scanner(System.in);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            Scanner sc = new Scanner(bufferedReader);
             int i = -1;
-            while(i < 0 || i >= selectables.size()){
-                if (getInstance().thread.isInterrupted()) {
-                    return;
+            try {
+                while (i < 0 || i >= selectables.size()) {
+                    while (!bufferedReader.ready()) {
+                        Thread.sleep(100);
+                    }
+                    System.out.println("Type a number between 0 and " + (selectables.size() - 1));
+                    if (sc.hasNextInt()) {
+                        i = sc.nextInt();
+                    } else {
+                        sc.next();
+                        i = -1;
+                    }
                 }
-                System.out.println("Type a number between 0 and " + (selectables.size() - 1));
-                if(sc.hasNextInt()){
-                    i = sc.nextInt();
-                } else {
-                    sc.next();
-                    i = -1;
-                }
+                onSelected.accept(i);
+            } catch(IOException|InterruptedException e){
+                // do nothing: thread closes
             }
-            onSelected.accept(i);
         });
 
         getInstance().thread.start();
@@ -146,9 +169,14 @@ public class InputManager {
 
     public static void closeInput(){
         try {
+            // If there is something in System.in, consume it
+            BufferedReader tmp = new BufferedReader(new InputStreamReader(System.in));
+            if (tmp.ready()) {
+                tmp.readLine();
+            }
             getInstance().thread.interrupt();
-        } catch(NullPointerException e){
-            // do nothing
+        } catch(NullPointerException|IOException e){
+            e.printStackTrace();
         }
     }
 }
